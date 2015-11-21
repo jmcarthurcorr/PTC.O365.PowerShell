c16024f7572cfe56e0ce2e82cdf0bc5add2fb31d
Sébastien Levert <slevert@sebastienlevert.com>
2015-11-21 09:53:57 -0500

    Adding missing files

--- Samples/07-Get-AccessToken.ps1
+++ Samples/07-Get-AccessToken.ps1
@@ -1,7 +1,6 @@
 . .\00-Constants.ps1
 . .\00-Initialize.ps1
 
-#$exchangeResourceUri = "https://outlook.office365.com/";
-$exchangeResourceUri = "https://graph.microsoft.com";
+$graphUri = "https://graph.microsoft.com";
 
-return Get-AccessToken -ResourceUri $exchangeResourceUri
+return Get-AccessToken -ResourceUri $graphUri
